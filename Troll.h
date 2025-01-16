dba793ac4174fec9b66e0668189810e3c9f9b9e9
rulerofspace <tlsalal1303@naver.com>
2025-01-16 17:46:41 +0900

    Add files via upload

---
+++ Troll.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "IMonster.h"
+
+class Troll : public IMonster
+{
+private:
+    Troll();
+    virtual ~Troll() = default;
+
+public:
+    virtual void Initialize(string name, double health, double attack) override;
+    virtual void Update() override;
+    //virtual void Interact() override;
+
+public:
+    static Troll* Create(string name, double health, double attack);
+    static Troll* CreateBoss(string name, double health, double attack);
+    virtual void Free() override;
+};
